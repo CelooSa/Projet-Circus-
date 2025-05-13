631ed6276b769f40551b64ea224ee4479ecaedc4
Célia S. <celoo.sa@icloud.com>
2025-05-13 12:00:06 +0200

    creation dossier backup

--- FRONT/src/App.jsx
+++ FRONT/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 
-import Register from './pages/auth/Register'; 
+//import Register2 from './pages/auth/Register2'; 
 import Home from './pages/Home';
 import SpectaclesFeu from './pages/SpectaclesFeu';
 import Artistes from './pages/Artists'; 
@@ -11,7 +11,7 @@ import Artistes from './pages/Artists';
 import './styles/Global.scss';
 import './styles/HomeStyle.scss';
 import './styles/ArtistsCarousel.scss';
-import './styles/Register.scss';
+//import './styles/Register2.scss';
 
 import './App.css';
 import VerifyEmail from './pages/VerifyEmail';
--- FRONT/src/pages/auth/Register2.jsx
+++ FRONT/src/pages/auth/Register2.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../../styles/Register.scss";
+import "../../styles/Register2.scss";
 
 const Register = () => {
   const [formData, setFormData] = useState({
